e0f93d08d51d9fbe3147754dd1b2435af31c67ad
jjarvik <janne.jarvikyla@efi.int>
2024-11-04 10:07:34 +0200

    Added network graph utility using ECharts

---
+++ core/network_graph.js
@@ -0,0 +1,278 @@
+// Initialize the echarts instance based on the prepared dom
+const chart = echarts.init(document.getElementById('main'));
+
+// [Absolute flow color, relative flow color]
+const edgeColors = ["rgba(59, 162, 114, 1)", "rgba(255, 50, 50, 1)"]
+
+const graphData = {
+  data: [],
+  link: [],
+  categories: []
+}
+
+
+// Created when script is built
+const yearToData = {year_to_data}
+first_year = Object.keys(yearToData)[0]
+const nodeData = yearToData[first_year]["node_index_to_data"]
+const edgeData = yearToData[first_year]["edge_index_to_data"]
+
+// Prebuilt mappings
+const processIdToDataIndex = new Map()
+
+let years = Object.keys(yearToData)
+console.log("yearToData: ", yearToData)
+
+// Get Node IDs
+const nodeIds = []
+for(const nodeIndex of Object.keys(nodeData)) {
+  const node = nodeData[nodeIndex]
+  nodeIds.push(node.process_id)
+}
+
+// Fill graphData.data
+for(const nodeIndex of Object.keys(nodeData)) {
+  const node = nodeData[nodeIndex]
+  const process_id = node.process_id
+  const newNodeData = {
+    name: process_id,
+    id: process_id,
+    category: process_id,
+    value: 0,
+    text: `Process ${process_id}`,
+  }
+
+  // Map process ID to data index
+  processIdToDataIndex.set(process_id, graphData.data.length)
+  graphData.data.push(newNodeData)
+}
+
+// Fill graphData.link
+for(const edgeIndex of Object.keys(edgeData)) {
+  const edge = edgeData[edgeIndex]
+  const flowId = edge.flow_id
+  const sourceProcessId = edge.source_process_id
+  const targetProcessId = edge.target_process_id
+  const isUnitAbsoluteValue = edge.is_unit_absolute_value
+  const value = edge.value
+  const unit = edge.unit
+  const newEdgeData = {
+    source: sourceProcessId,
+    target: targetProcessId,
+    label: {
+      show: true,
+      position: "middle",
+      formatter: (params) => {
+        return isUnitAbsoluteValue ? "ABS" : "%"
+      }
+    },
+    lineStyle: {
+      color: isUnitAbsoluteValue ? edgeColors[0] : edgeColors[1]
+
+    },
+
+    // Custom data
+    text: isUnitAbsoluteValue ? "Absolute flow" : "Relative flow",
+    value: `${value} ${unit}`
+  }
+  graphData.link.push(newEdgeData)
+}
+
+// Fill graphData.categories
+for(const node of graphData.data) {
+  const newCategoryData = {
+    name: node.id
+  }
+  graphData.categories.push(newCategoryData)
+}
+
+const legendData = nodeIds
+
+// Specify the configuration items and data for the chart
+const option = {
+  baseOption: {
+    title: {
+      text: 'Process connections'
+    },
+    tooltip: {
+      formatter: function (params) {
+        if (params.dataType == 'node') {
+          return
+
+          // let text = params.data.text
+          // return `<div>${text}</div>`
+        }
+
+        if (params.dataType === 'edge') {
+          let text = params.data.text
+          let value = params.data.value
+          return `<div>${text}</div><div>${value}</div>`
+        }
+      }
+    },
+    legend: [
+      {
+        type: 'scroll',
+        data: legendData,
+        position: 'left',
+        orient: 'vertical',
+        right: 10,
+        top: 50,
+        height: "88%",
+      }
+    ],
+    timeline: {
+      show: true,
+      type: "slider",
+      axisType: 'category',
+      data: years,
+      left: '20px',
+      right: '20px',
+    },
+    series: [
+      {
+        name: 'Process flows',
+        type: 'graph',
+        layout: 'force',
+        data: graphData.data,
+        links: graphData.link,
+        categories: graphData.categories,
+        zoom: 2,
+        draggable: true,
+        symbolSize: 40,
+        symbol: 'circle', // 'rect'
+        label: {
+          show: true // node name to be shown in circle
+        },
+        edgeSymbol: ['circle', 'arrow'], // for arrow from one to another
+        edgeSymbolSize: [0, 15],
+
+        emphasis: {
+          focus: 'adjacency',
+          label: {
+            show: true
+          },
+          // disabled: true,
+        },
+        roam: true,
+        force: {
+          repulsion: [500, 1000, 2000],
+          edgeLength: 50,
+        }
+      }
+    ]
+  }, options: [],
+}
+
+// Display the chart using the configuration items and data just specified.
+chart.setOption(option);
+
+// Resize chart when window size has changed
+addEventListener("resize", (e) => {
+  chart.resize()
+})
+
+chart.on("timelinechanged", function (params) {
+  console.log(params)
+  const year = years[params.currentIndex]
+  console.log(year)
+})
+
+// chart.on("highlight", (e) => {
+//   // console.log("Highlighting ", e)
+// })
+
+
+let selectedNodes = []
+// chart.on("select", (e) => {
+//   // console.log("Select: ", e)
+//   const dataType = e.dataType // "node" or "edge"?
+//   const seriesIndex = e.seriesIndex
+//   const dataIndexInside = e.dataIndexInside
+//
+//   // Update options
+//   const option = chart.getOption()
+//   const series = option.series[seriesIndex]
+//   const newOption = {
+//       series: [
+//         {
+//           type: "graph",
+//           emphasis: {
+//             disabled: false,
+//           }
+//         }]
+//   }
+//
+//   chart.setOption(newOption)
+// })
+
+chart.on("click", { dataType: "node" }, (params) => {
+  console.log(params)
+
+  const dataType = params.dataType
+  const seriesIndex = params.seriesIndex
+  const dataIndex = params.dataIndex
+  selectedNodes.push({ seriesIndex: seriesIndex, dataIndex: dataIndex })
+})
+
+chart.on("click", { dataType: "edge" }, (params) => {
+  console.log(params)
+})
+
+// chart.on("mouseover", (params) => {
+//   // console.log(params)
+//   console.log("Mouse entering")
+//
+//   const nodeDataIndex = []
+//   const edgeDataIndex = []
+//   for(const entry of selectedNodes) {
+//     nodeDataIndex.push(entry.dataIndex)
+//   }
+//
+//   const option = chart.getOption()
+//   for(const entry of selectedNodes) {
+//     const node = option.series[entry.seriesIndex].data[entry.dataIndex]
+//     const links = option.series[entry.seriesIndex].links
+//     console.log(node)
+//
+//     console.log(entry.dataIndex, processIdToDataIndex.get(node.id))
+//
+//     // Select all adjacent edges to node
+//     for(const [edgeIndex, edge] of links.entries()) {
+//       // console.log(edgeIndex, edge)
+//
+//       // Inflows to node
+//       if(edge.target == node.id) {
+//         edgeDataIndex.push(edgeIndex)
+//
+//         // Select source node also
+//         const sourceDataIndex = processIdToDataIndex.get(edge.source)
+//         nodeDataIndex.push(sourceDataIndex)
+//       }
+//
+//       // Outflow from node
+//       if(edge.source == node.id) {
+//         edgeDataIndex.push(edgeIndex)
+//
+//         // Select target node also
+//         const targetDataIndex = processIdToDataIndex.get(edge.target)
+//         nodeDataIndex.push(targetDataIndex)
+//       }
+//
+//     }
+//   }
+//
+//   chart.dispatchAction({
+//     type: 'highlight',
+//     batch: [
+//       { dataType: 'node', dataIndex: nodeDataIndex},
+//       { dataType: 'edge', dataIndex: edgeDataIndex, notBlur: true},
+//     ],
+//   })
+// })
+
+// chart.on("mouseout", (params) => {
+//   // console.log(params)
+//   console.log("Mouse leaving")
+// })
+
---
+++ core/network_graph.py
@@ -0,0 +1,85 @@
+import webbrowser
+import os
+import json
+import pandas as pd
+from typing import List, Dict
+
+
+class NetworkGraph(object):
+    def __init__(self):
+        self._echarts = ""
+        self._visualizer = ""
+        self._html = ""
+
+        # Load ECharts from file
+        with open("./core/echarts_min.js", mode="r", encoding="utf-8") as fs:
+            self._echarts = fs.read()
+
+        # Load visualizer script from file
+        with open("./core/network_graph.js", mode="r", encoding="utf-8") as fs:
+            self._visualizer = fs.read()
+
+    def build(self, df_process_to_flows: pd.DataFrame, years_to_check: List[int]):
+        """
+        Build HTML file with ECharts visualization library.
+
+        :param df_process_to_flows:
+        :param years_to_check:
+        :return:
+        """
+        year_to_data = {}
+        for year in years_to_check:
+            rows = df_process_to_flows.loc[year]
+            node_index_to_data = {}
+            edge_index_to_data = {}
+
+            # Create nodes and edges
+            new_node_index = 0
+            new_edge_index = 0
+            for row in rows:
+                process = row["process"]
+                outflows = row["flows"]["out"]
+
+                # Create node data
+                new_node_data = {
+                    "process_id": process.id,
+                    "transformation_stage": process.transformation_stage,
+                }
+                node_index_to_data[new_node_index] = new_node_data
+                new_node_index += 1
+
+                # Create edges from nodes (= process outflows)
+                for flow in outflows:
+                    new_edge_data = {
+                        "flow_id": flow.id,
+                        "source_process_id": flow.source_process_id,
+                        "target_process_id": flow.target_process_id,
+                        "is_unit_absolute_value": flow.is_unit_absolute_value,
+                        "value": flow.value,
+                        "unit": flow.unit,
+                    }
+                    edge_index_to_data[new_edge_index] = new_edge_data
+                    new_edge_index += 1
+
+            year_to_data[year] = {
+                "node_index_to_data": node_index_to_data,
+                "edge_index_to_data": edge_index_to_data,
+            }
+
+        # Replace data in visualizer
+        script = self._visualizer
+        script = script.replace("{year_to_data}", json.dumps(year_to_data))
+        self._visualizer = script
+
+        with open("./core/network_graph.html", "r", encoding="utf-8") as fs:
+            self._html = fs.read()
+
+        # Replace 'echarts_content' and 'visualizer
+        self._html = self._html.replace("{echarts_content}", self._echarts)
+        self._html = self._html.replace("{visualizer_content}", self._visualizer)
+
+    def show(self):
+        filename = "export.html"
+        with open(filename, "w", encoding="utf-8") as fs:
+            fs.write(self._html)
+        webbrowser.open("file://" + os.path.realpath(filename))
