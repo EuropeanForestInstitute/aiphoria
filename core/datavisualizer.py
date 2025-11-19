a18743593702886cc46620760ab86138aeda145d
jjarvik <janne.jarvikyla@efi.int>
2025-11-19 15:26:07 +0200

    Updated: - Sankey graph allows now to inspect detailed data for processes, flows, and stocks - Context menu system for processes, flows, and stocks (right-click in either list view in "Data"-tab or from Sankey graph - Stock data is also included in Sankey graph result HTML file - Added option to change Sankey graph font size - Implement combining multiple scenario data to one HTML file - Added dropdown to select between scenarios

--- core/datavisualizer.py
+++ core/datavisualizer.py
@@ -35,8 +35,23 @@ class DataVisualizer(object):
             scenario_name_to_data[scenario.name] = self._build_scenario_year_to_data(scenario, visualizer_params)
 
         if combine_to_one_file:
-            # NOTE: Implement for the next version
-            print("Combine to one file not implemented yet")
+            # Build combined output file that contains all scenarios
+            for scenario in scenarios:
+                scenario_name_to_info[scenario.name] = self._build_scenario_info(scenario)
+                scenario_name_to_data[scenario.name] = self._build_scenario_year_to_data(scenario, visualizer_params)
+
+            # Generate HTML file for scenarios
+            html = self._build_combined_scenario_graph(scenario_name_to_info, scenario_name_to_data, visualizer_params)
+
+            output_path = model_params[ParameterName.OutputPath]
+            output_filename = "combined_sankey.html"
+            abs_path_to_file = os.path.join(output_path, output_filename)
+
+            with open(abs_path_to_file, "w", encoding="utf-8") as fs:
+                fs.write(html)
+
+            if model_params[ParameterName.ShowPlots]:
+                webbrowser.open("file://" + os.path.realpath(abs_path_to_file))
 
         else:
             # Build separate files for each scenario
@@ -163,6 +178,8 @@ class DataVisualizer(object):
                     # Custom data for link
                     year_link_custom_data.append(
                         dict(
+                            source_process_id=flow.source_process_id,
+                            target_process_id=flow.target_process_id,
                             is_visible=True,
                             is_virtual=flow.is_virtual,
                             evaluated_value=flow.evaluated_value,
@@ -207,6 +224,52 @@ class DataVisualizer(object):
                 "node_positions_y": year_node_positions_y,
                 "node_custom_data": year_node_custom_data
             }
+
+        # Make list of indicator names
+        stock_indicator_names = [baseline_value_name]
+        stock_indicator_units = [baseline_unit_name]
+        for indicator_name, indicator_entry in scenario.scenario_data.indicator_name_to_indicator.items():
+            stock_indicator_names.append(indicator_name)
+            stock_indicator_units.append(indicator_entry.unit)
+
+        # Unpack stock data to yearly values
+        dsm_baselines = flow_solver.get_baseline_dynamic_stocks()
+        dsm_indicators = flow_solver.get_indicator_dynamic_stocks()
+        for year_index, year in enumerate(year_to_data.keys()):
+            stock_id_to_stock_inflow = {}
+            stock_id_to_stock_outflow = {}
+            stock_id_to_stock_total = {}
+            stock_ids = [stock_id for stock_id in dsm_baselines.keys()]
+            for stock_id in stock_ids:
+                stock_id_to_stock_inflow[stock_id] = []
+                stock_id_to_stock_outflow[stock_id] = []
+                stock_id_to_stock_total[stock_id] = []
+
+                process_dsm_baseline = dsm_baselines[stock_id]
+                dsm_inflows = process_dsm_baseline.i
+                dsm_outflows = process_dsm_baseline.o
+                dsm_total = process_dsm_baseline.s
+                stock_id_to_stock_inflow[stock_id].append(dsm_inflows[year_index])
+                stock_id_to_stock_outflow[stock_id].append(dsm_outflows[year_index])
+                stock_id_to_stock_total[stock_id].append(dsm_total[year_index])
+                for indicator_name, process_dsm_indicator in dsm_indicators[stock_id].items():
+                    dsm_inflows = process_dsm_indicator.i
+                    dsm_outflows = process_dsm_indicator.o
+                    dsm_total = process_dsm_indicator.s
+                    stock_id_to_stock_inflow[stock_id].append(dsm_inflows[year_index])
+                    stock_id_to_stock_outflow[stock_id].append(dsm_outflows[year_index])
+                    stock_id_to_stock_total[stock_id].append(dsm_total[year_index])
+
+            stock_data = {
+                "stock_ids": stock_ids,
+                "stock_indicator_names": stock_indicator_names,
+                "stock_indicator_units": stock_indicator_units,
+                "stock_inflows": stock_id_to_stock_inflow,
+                "stock_outflows": stock_id_to_stock_outflow,
+                "stock_totals": stock_id_to_stock_total,
+            }
+            for key in stock_data.keys():
+                year_to_data[year][key] = stock_data[key]
 
         return year_to_data
 
@@ -255,6 +318,49 @@ class DataVisualizer(object):
         # Scenario year to data as JSON
         html = html.replace("// rawYearToData:", "rawYearToData:")
         html = html.replace("{rawYearToData}", json.dumps(scenario_data))
+
+        return html
+
+    def _build_combined_scenario_graph(self,
+                                       scenario_name_to_info: Dict[str, Any],
+                                       scenario_name_to_data: Dict[str, Any] = None,
+                                       params: Dict = None):
+
+        # Add JS script that is run after the Plotly has loaded
+        filename_plotly = os.path.join(os.path.abspath("."), "core", "datavisualizer_data/plotly-3.0.0.min.js")
+        filename_html = os.path.join(os.path.abspath("."), "core", "datavisualizer_data/datavisualizer_plotly.html")
+
+        # Read HTML file contents
+        html = ""
+        with open(filename_html, "r", encoding="utf-8") as fs:
+            html = fs.read()
+
+        # Read PlotlyJS file contents
+        plotly_js = ""
+        with open(filename_plotly, "r", encoding="utf-8") as fs:
+            plotly_js = fs.read()
+
+        # Replace contents with data
+        # Plotly
+        html = html.replace(
+            '<script src="./plotly-3.0.0.min.js"></script>',
+            f'<script type="text/javascript">{plotly_js}</script>')
+
+        # # Scenario info as JSON
+        # html = html.replace("// rawScenarioInfo:", "rawScenarioInfo:")
+        # html = html.replace("{rawScenarioInfo}", json.dumps(scenario_info))
+
+        # # Scenario year to data as JSON
+        # html = html.replace("// rawYearToData:", "rawYearToData:")
+        # html = html.replace("{rawYearToData}", json.dumps(scenario_data))
+
+        # Scenario name to scenario info as JSON
+        html = html.replace("// rawScenarioNameToInfo:", "rawScenarioNameToInfo:")
+        html = html.replace("{rawScenarioNameToInfo}", json.dumps(scenario_name_to_info))
+
+        # Scenario name to scenario data as JSON
+        html = html.replace("// rawScenarioNameToData:", "rawScenarioNameToData:")
+        html = html.replace("{rawScenarioNameToData}", json.dumps(scenario_name_to_data))
 
         return html
 
